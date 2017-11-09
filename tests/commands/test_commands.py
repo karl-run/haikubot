df8afd937d1f4e135e1178640b5ac41dd45b7ab4
Karl J. Overå <karloveraa@gmail.com>
2017-11-09 21:59:42 +0100

    Fixed broken test

--- tests/commands/test_commands.py
+++ tests/commands/test_commands.py
@@ -7,5 +7,6 @@ class CommandsTest(unittest.TestCase):
     def test_values(self):
         values = Commands.values()
         good = ['add mod', 'remove mod', 'list mod', 'show last', 'show',
-                'show from', 'export', 'stats top', 'add haiku', 'delete haiku']
+                'show from', 'export', 'stats top', 'stats longest', 'stats fewest', 'stats most',
+                'add haiku', 'delete haiku']
         self.assertEqual(good, values)
